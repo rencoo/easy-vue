f4e50fc7f44764f6f14a0a7514d1d9a7c4e6305f
rencoo <rencoogle@outlook.com>
2019-12-08 22:31:03 +0800

    Add vm method  related to data based on Watcher

--- vue.js
+++ vue.js
@@ -1,8 +1,8 @@
 import { Compile } from './compile/index.js'
-import { Observer } from './observer/index.js' 
+import { Observer, Watcher } from './observer/index.js' 
 
 function Vue (options) {
-    this.options = options; // 用于查找methods等非数据属性
+    this.$options = options; // 用于查找methods等非数据属性
 
     // vm数据属性改变, 视图做出响应
     new Observer(options.data, this); 
@@ -10,5 +10,10 @@ function Vue (options) {
     // 从根节点开始编译HTML
     new Compile(document.querySelector(options.el), this);
 }
+
+Vue.prototype.$watch = function (expOrFn, cb, options) {
+    var vm = this;
+    new Watcher(vm, expOrFn, cb, options);
+};
 
 export { Vue }
